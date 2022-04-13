23a6e6c67d3e95e6e580821a721195ada94c50ae
newbei <hegang5862@qq.com>
2022-04-13 11:06:08 +0800

    Fix data_path for test_loader.py.

--- tsbenchmark/tests/test_tsloader.py
+++ tsbenchmark/tests/test_tsloader.py
@@ -1,8 +1,9 @@
 import os
+import tsbenchmark
 from tsbenchmark.tsloader import TSDataSetLoader, TSTaskLoader
 
-data_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'datas')
-dataloader = TSDataSetLoader(data_path)
+data_path = os.path.join(os.path.dirname(os.path.dirname(tsbenchmark.__file__)), 'datas')
+dataloader = tsbenchmark.tsloader.TSDataSetLoader(data_path)
 
 
 class Test_TSDataSetLoader():
@@ -17,9 +18,9 @@ class Test_TSDataSetLoader():
         assert dataloader.exists(694826) == True
         assert dataloader.exists(9527) == False
 
-    # def test_load(self):
-    #     df_train, df_test, metadata = dataloader.load(694826)
-    #     assert df_train.shape[0] == 124 and df_test.shape[0] == 6 and len(metadata) == 15
+    def test_load(self):
+        df_train, df_test, metadata = dataloader.load(694826)
+        assert df_train.shape[0] == 124 and df_test.shape[0] == 6 and len(metadata) == 15
 
 
 taskloader = TSTaskLoader(data_path)
@@ -35,6 +36,6 @@ class Test_TSTaskLoader():
         assert taskloader.exists(694826) == True
         assert taskloader.exists(9527) == False
 
-    # def test_load(self):
-    #     task = taskloader.load(694826)
-    #     assert task.task == 'univariate-forecast' and task.dataset_id == 694826
+    def test_load(self):
+        task = taskloader.load(694826)
+        assert task.task == 'univariate-forecast' and task.dataset_id == 694826
