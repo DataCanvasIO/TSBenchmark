b9eeb790150ef5c70d6c55cb23ebaf77297b3732
newbei <hegang5862@qq.com>
2022-05-04 13:40:40 +0800

    Integrated report module and parallel calculation.

---
+++ tsbenchmark/tests/players/hyperts_stat_player/exec.py
@@ -0,0 +1,68 @@
+import tsbenchmark as tsb
+import tsbenchmark.api
+import time
+from hyperts.utils import metrics
+
+
+def cal_metric(y_pred, y_test, date_col_name, series_col_name, covariables, metrics_target, task_calc_score):
+    if series_col_name != None:
+        y_pred = y_pred[series_col_name]
+        y_test = y_test[series_col_name]
+
+    if date_col_name in y_pred.columns:
+        y_pred = y_pred.drop(columns=[date_col_name], axis=1)
+    if date_col_name in y_test.columns:
+        y_test = y_test.drop(columns=[date_col_name], axis=1)
+    if covariables != None:
+        y_test = y_test.drop(columns=[covariables], axis=1)
+    hypertsmetric = metrics.calc_score(y_test, y_pred,
+                                       metrics=metrics_target, task=task_calc_score)
+    return hypertsmetric
+
+
+def main():
+    task = tsb.api.get_task()
+    from hyperts.experiment import make_experiment
+    train_df = task.get_train().copy(deep=True)
+    time_start = time.time()
+    print('train_begin')
+    print(task.taskdata.taskdata_loader.dataset_loader.data_path)
+
+    exp = make_experiment(train_df,
+                          timestamp=task.date_name,
+                          task=task.task,
+                          reward_metric=task.reward_metric,
+                          timestamp_format=task.dtformat,
+                          covariables=task.covariables_name,
+                          max_trials=1, #todo
+                          optimize_direction='min',
+                          verbose=1,
+                          log_level='INFO',
+                          early_stopping_rounds=30,
+                          early_stopping_time_limit=0,
+                          random_state=task.random_state
+                          )
+    print('train_end')
+    model = exp.run()
+    X_test, y_test = model.split_X_y(task.get_test().copy())
+    y_pred = model.predict(X_test)
+
+    metrics = cal_metric(y_pred, task.get_test(), task.date_name, task.series_name,
+               task.covariables_name,
+               ['smape', 'mape', 'rmse', 'mae'], 'regression')
+
+
+    duration = time.time() - time_start
+
+    report_data = {
+        'duration': duration,
+        'y_predict':y_pred[task.series_name].to_json(orient='records')[1:-1].replace('},{', '} {'),
+        'y_real':y_test.to_json(orient='records')[1:-1].replace('},{', '} {'),
+        'metrics': metrics,
+        'data_path': task.taskdata.taskdata_loader.dataset_loader.data_path
+    }
+    tsb.api.report_task(report_data=report_data)
+
+
+if __name__ == "__main__":
+    main()
--- tsbenchmark/tests/test_reporter.py
+++ tsbenchmark/tests/test_reporter.py
@@ -6,11 +6,29 @@ from tsbenchmark.tasks import TSTask
 from tsbenchmark.reporter import Reporter
 from hypernets.utils import logging
 
+import os
+import tempfile
+from typing import Dict
+from pathlib import Path
+
+import pytest
+
+from hypernets.hyperctl.appliation import BatchApplication
+from hypernets.hyperctl.batch import ShellJob
+from hypernets.tests.hyperctl.test_scheduler import assert_batch_finished
+from hypernets.utils import ssh_utils
+from tsbenchmark.benchmark import LocalBenchmark, load_players, RemoteSSHBenchmark
+from tsbenchmark.callbacks import BenchmarkCallback
+from tsbenchmark.players import load_player
+from tsbenchmark.tasks import TSTask
+from hypernets.tests.utils import ssh_utils_test
+
 logging.set_level('DEBUG')  # TODO
 logger = logging.getLogger(__name__)
 
 import tsbenchmark.tasks
 
+HERE = Path(__file__).parent
 
 def create_tasks_new():
     tasks = [TSTask(tsbenchmark.tasks.get_task_config(t_id), random_state=8086, max_trails=1, reward_metric='rmse') for
@@ -20,8 +38,8 @@ def create_tasks_new():
 
 def create_benchmark_cfg():
     benchmark_config = {'report.path': '/tmp/report_path',
-                        'name': 'hyperts_release_0.4.0',
-                        'desc': 'develop for pytest hyperts_release_0.3.0',
+                        'name': 'report_remote',
+                        'desc': 'report_remote',
                         'random_states': [8086],
                         'task_filter.tasks': ['univariate-forecast']
                         }
@@ -75,3 +93,83 @@ def atest_reporter_generate():
     benchmark_config = create_benchmark_cfg()
     rc = ReporterCallback(benchmark_config=benchmark_config)
     rc.reporter.generate_report()
+
+def get_conda_home():
+    return os.getenv("TSB_CONDA_HOME")
+
+
+def _conda_ready():
+    conda_home = get_conda_home()
+    if conda_home is not None:
+        return Path(conda_home).exists()
+    else:
+        return False
+
+# export TSB_CONDA_HOME=/opt/miniconda3
+need_conda = pytest.mark.skipif(not _conda_ready(),
+                                reason='The test case need conda to be installed and set env "TSB_CONDA_HOME"')
+
+need_private_pypi = pytest.mark.skipif(os.getenv("TSB_PYPI") is None,
+                                       reason='The test case need a private pypi to install requirements"')
+
+need_server_host = pytest.mark.skipif(os.getenv("TSB_SERVER_HOST") is None,
+                                      reason='The test case need to set env "TSB_SERVER_HOST"')
+
+def create_task():
+    task_config_id = 694826
+    task_config = tsbenchmark.tasks.get_task_config(task_config_id)
+    return task_config
+
+@ssh_utils_test.need_psw_auth_ssh
+@need_server_host
+class TestRemoteCustomPythonBenchmark:
+    """Benchmark with constraints:
+        - remote benchmark
+        - custom python
+    """
+
+    def setup_class(self):
+        self.connection = ssh_utils_test.load_ssh_psw_config()
+        players = load_players([(HERE / "players" / "hyperts_stat_player").as_posix(), (HERE / "players" / "hyperts_dl_player").as_posix()])
+        task0 = create_task()
+        benchmark_config = create_benchmark_cfg()
+        rc = ReporterCallback(benchmark_config=benchmark_config)
+        callbacks = [rc]
+        self.working_dir_path = Path(tempfile.mkdtemp(prefix="benchmark-test-batches"))
+        self.benchmark_name = 'remote-benchmark'
+
+        lb = RemoteSSHBenchmark(name=self.benchmark_name, desc='desc', players=players,
+                                random_states=[8086], ts_tasks_config=[task0],
+                                working_dir=self.working_dir_path.as_posix(),
+                                scheduler_exit_on_finish=True,
+                                server_host=os.getenv('TSB_SERVER_HOST'),  # external ip
+                                constraints={}, callbacks=callbacks,
+                                machines=[self.connection])
+        self.lb = lb
+
+    def atest_run_benchmark(self):
+        self.lb.run()
+
+        # assert local files
+        batch_path = self.working_dir_path / "batches" / self.benchmark_name
+        assert batch_path.exists()
+        batch_app: BatchApplication = self.lb._batch_app
+        jobs = batch_app.batch.jobs
+        assert len(jobs) == 2
+        job = jobs[0]
+        # job succeed
+        assert (batch_path / f"{job.name}.succeed").exists()
+
+        # assert remote files
+        job_working_dir_path = batch_path / job.name
+        with ssh_utils.sftp_client(**self.connection) as client:
+            # working dir
+            assert ssh_utils.exists(client, job_working_dir_path.as_posix())
+            # runpy.sh
+            assert ssh_utils.exists(client, (job_working_dir_path / "resources" / "runpy.sh").as_posix())
+            # # player
+            # assert ssh_utils.exists(client, (job_working_dir_path / "resources" / "hyperts_dl_player" / "exec.py").as_posix())
+            # assert ssh_utils.exists(client, (job_working_dir_path / "resources" / "hyperts_dl_player" / "player.yaml").as_posix())
+
+    def teardown_class(self):
+        self.lb.stop()
