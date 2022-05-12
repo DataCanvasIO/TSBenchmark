cec2f6c723aced2854fdfb11de5a1532babfc43d
Haifeng Wu <oaksharks@outlook.com>
2022-05-12 11:42:09 +0800

    enable task selected

--- tsbenchmark/api.py
+++ tsbenchmark/api.py
@@ -8,8 +8,6 @@ import json
 import os
 import time
 
-import requests
-
 from hypernets.hyperctl import consts
 from hypernets.hyperctl import utils
 
@@ -128,32 +126,3 @@ def _get_bm_task_id(bm_task_id):
     else:
         return bm_task_id
 
-
-def _get_job_name_and_damon_portal():
-    job_name = os.getenv(consts.KEY_ENV_JOB_NAME)
-    api_server_portal = f"{os.getenv(consts.KEY_ENV_SERVER_PORTAL)}"
-
-    assert job_name
-    assert api_server_portal
-
-    return job_name, api_server_portal
-
-
-def get_job_working_dir():
-    job_working_dir = os.getenv(consts.KEY_ENV_JOB_WORKING_DIR)
-    return job_working_dir
-
-
-def list_jobs(api_server_portal):
-    # if api_server_portal is None:
-    #     api_server_portal = os.getenv(consts.KEY_ENV_api_server_portal)
-    assert api_server_portal
-    url_get_jobs = f"{api_server_portal}/hyperctl/api/job"
-    data = _request(url_get_jobs)
-    return data['jobs']
-
-
-def kill_job(api_server_portal, job_name):
-    url_kill_job = f"{api_server_portal}/hyperctl/api/job/{job_name}/kill"
-    data = _request(url_kill_job, method='post')
-    return data
